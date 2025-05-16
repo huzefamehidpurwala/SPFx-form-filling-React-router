6c1e967edc67c8bfa1bafb6c0ba86fde6f4f0a6b
huzefaPSSPL <huzefa.mehidpurwala@prakashinfotech.com>
2025-05-16 13:29:27 +0530

    first commit

---
+++ src/webparts/workflowBillApproval/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IWorkflowBillApprovalWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'WorkflowBillApprovalWebPartStrings' {
+  const strings: IWorkflowBillApprovalWebPartStrings;
+  export = strings;
+}
