5900af5e635075715e0d701e3dd43072c85167ef
huzefaPSSPL <huzefa.mehidpurwala@prakashinfotech.com>
2025-05-16 16:09:54 +0530

    implement react router dummy check

--- src/webparts/workflowBillApproval/components/WorkflowBillApproval.tsx
+++ src/webparts/workflowBillApproval/components/WorkflowBillApproval.tsx
@@ -1,17 +1,34 @@
 import * as React from "react";
 import styles from "./WorkflowBillApproval.module.scss";
 import type { IWorkflowBillApprovalProps } from "./IWorkflowBillApprovalProps";
-import { escape } from "@microsoft/sp-lodash-subset";
+// import { escape } from "@microsoft/sp-lodash-subset";
+import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
+
+function App() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <p>i am in app comp</p>
+      {/* <a href="#/about">about</a> */}
+      <button type="button" onClick={() => navigate("/about")}>
+        about
+      </button>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <a href="#/">home</a>
+      <p>i am in about comp</p>
+    </div>
+  );
+}
 
 export default class WorkflowBillApproval extends React.Component<IWorkflowBillApprovalProps> {
   public render(): React.ReactElement<IWorkflowBillApprovalProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName,
-    } = this.props;
+    const { hasTeamsContext } = this.props;
 
     return (
       <section
@@ -19,89 +36,12 @@ export default class WorkflowBillApproval extends React.Component<IWorkflowBillA
           hasTeamsContext ? styles.teams : ""
         }`}
       >
-        <div className={styles.welcome}>
-          <img
-            alt=""
-            src={
-              isDarkTheme
-                ? require("../assets/welcome-dark.png")
-                : require("../assets/welcome-light.png")
-            }
-            className={styles.welcomeImage}
-          />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>
-            Web part property value: <strong>{escape(description)}</strong>
-          </div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for
-            Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest
-            way to extend Microsoft 365 with automatic Single Sign On, automatic
-            hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li>
-              <a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">
-                SharePoint Framework Overview
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://aka.ms/spfx-yeoman-graph"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Use Microsoft Graph in your solution
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://aka.ms/spfx-yeoman-teams"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Build for Microsoft Teams using SharePoint Framework
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://aka.ms/spfx-yeoman-viva"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Build for Microsoft Viva Connections using SharePoint Framework
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://aka.ms/spfx-yeoman-store"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Publish SharePoint Framework applications to the marketplace
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://aka.ms/spfx-yeoman-api"
-                target="_blank"
-                rel="noreferrer"
-              >
-                SharePoint Framework API reference
-              </a>
-            </li>
-            <li>
-              <a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">
-                Microsoft 365 Developer Community
-              </a>
-            </li>
-          </ul>
-        </div>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </HashRouter>
       </section>
     );
   }
