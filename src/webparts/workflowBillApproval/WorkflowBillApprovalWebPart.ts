54e8d9b1d0b6532c5b3121f27b4869524a3eb9f6
huzefaPSSPL <huzefa.mehidpurwala@prakashinfotech.com>
2025-05-22 16:42:00 +0530

    remove propertyPane configuration

--- src/webparts/workflowBillApproval/WorkflowBillApprovalWebPart.ts
+++ src/webparts/workflowBillApproval/WorkflowBillApprovalWebPart.ts
@@ -1,21 +1,15 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import { Version } from "@microsoft/sp-core-library";
-import {
-  type IPropertyPaneConfiguration,
-  PropertyPaneTextField,
-} from "@microsoft/sp-property-pane";
 import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 import { IReadonlyTheme } from "@microsoft/sp-component-base";
 
-import * as strings from "WorkflowBillApprovalWebPartStrings";
+// import * as strings from "WorkflowBillApprovalWebPartStrings";
 import WorkflowBillApproval from "./components/WorkflowBillApproval";
 import { IWorkflowBillApprovalProps } from "./components/IWorkflowBillApprovalProps";
 import { sp } from "@pnp/sp";
 
-export interface IWorkflowBillApprovalWebPartProps {
-  description: string;
-}
+export interface IWorkflowBillApprovalWebPartProps {}
 
 export default class WorkflowBillApprovalWebPart extends BaseClientSideWebPart<IWorkflowBillApprovalWebPartProps> {
   private _isDarkTheme: boolean = false;
@@ -39,48 +33,6 @@ export default class WorkflowBillApprovalWebPart extends BaseClientSideWebPart<I
     sp.setup({
       spfxContext: this.context, // Wire up SPFx authentication
     });
-    return this._getEnvironmentMessage().then((message) => {
-      this._environmentMessage = message;
-    });
-  }
-
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) {
-      // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app
-        .getContext()
-        .then((context) => {
-          let environmentMessage: string = "";
-          switch (context.app.host.name) {
-            case "Office": // running in Office
-              environmentMessage = this.context.isServedFromLocalhost
-                ? strings.AppLocalEnvironmentOffice
-                : strings.AppOfficeEnvironment;
-              break;
-            case "Outlook": // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost
-                ? strings.AppLocalEnvironmentOutlook
-                : strings.AppOutlookEnvironment;
-              break;
-            case "Teams": // running in Teams
-            case "TeamsModern":
-              environmentMessage = this.context.isServedFromLocalhost
-                ? strings.AppLocalEnvironmentTeams
-                : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              environmentMessage = strings.UnknownEnvironment;
-          }
-
-          return environmentMessage;
-        });
-    }
-
-    return Promise.resolve(
-      this.context.isServedFromLocalhost
-        ? strings.AppLocalEnvironmentSharePoint
-        : strings.AppSharePointEnvironment
-    );
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -110,27 +62,5 @@ export default class WorkflowBillApprovalWebPart extends BaseClientSideWebPart<I
 
   protected get dataVersion(): Version {
     return Version.parse("1.0");
-  }
-
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription,
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField("description", {
-                  label: strings.DescriptionFieldLabel,
-                }),
-              ],
-            },
-          ],
-        },
-      ],
-    };
   }
 }
