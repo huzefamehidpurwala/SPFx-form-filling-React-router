c6b2497ca0af4919836b99e44b10ccf83baea3b2
huzefaPSSPL <huzefa.mehidpurwala@prakashinfotech.com>
2025-05-26 14:44:11 +0530

    implement react-query in the FormsList comp

--- src/webparts/workflowBillApproval/components/Forms/FormsList.tsx
+++ src/webparts/workflowBillApproval/components/Forms/FormsList.tsx
@@ -11,6 +11,8 @@ import { sp } from "@pnp/sp";
 import { listId } from "./Form";
 import { ContextStore } from "../Context/ContextStore";
 import styles from "../WorkflowBillApproval.module.scss";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../../helper/Loading";
 
 export interface IDetailsListBasicExampleItem {
   key: number;
@@ -139,100 +141,114 @@ const columns: IColumn[] = [
   },
 ];
 
+const fetchFormList = async (
+  currUserEmail: string
+): Promise<IDetailsListBasicExampleItem[]> => {
+  const itemListPr = sp.web.lists
+    .getById(listId)
+    .items.expand("Author")
+    .select("Author/EMail", "Id", "location", "plantCode", "currStep")
+    .filter("currStep le 3")
+    .get();
+
+  // Get all groups for the current user
+  const userGroupsPr = sp.web.currentUser.groups.select("Title").get(); // : Array<{ Title: string }>
+
+  const [itemList, userGroups] = (await Promise.all([
+    itemListPr,
+    userGroupsPr,
+  ])) as [
+    {
+      Id: number;
+      location: string;
+      plantCode: string;
+      currStep: number;
+    }[],
+    { Title: string }[]
+  ];
+
+  let currUserGrpNum = 0;
+  let currUserGmLocation = "";
+  userGroups.forEach((grp) => {
+    if (grp.Title === "GM QC") {
+      currUserGrpNum = 3;
+    } else if (grp.Title === "PP Department") {
+      currUserGrpNum = 2;
+    } else if (grp.Title.toLowerCase().includes("gm")) {
+      currUserGrpNum = 1;
+      currUserGmLocation = grp.Title.toLowerCase().replace("gm ", "");
+    }
+  });
+
+  return itemList.map((item) => {
+    const rCurrStep = item.currStep as number;
+    const autherEMail = (item as any).Author.EMail as string;
+
+    let isAuthorized = false;
+    switch (rCurrStep) {
+      case 1:
+        isAuthorized =
+          currUserGrpNum === 1 &&
+          currUserGmLocation === item.location.toLowerCase();
+        break;
+
+      case 2:
+        isAuthorized = rCurrStep === currUserGrpNum;
+        break;
+
+      case 3:
+        isAuthorized = rCurrStep === currUserGrpNum;
+        break;
+
+      case 0:
+        isAuthorized =
+          autherEMail.toLowerCase() === currUserEmail.toLowerCase();
+        break;
+
+      default:
+        break;
+    }
+
+    return {
+      ...item,
+      key: item.Id,
+      isAuthorized: Number(isAuthorized),
+    };
+  });
+};
+
 const FormsList: React.FC = () => {
   const { spContext: context } = React.useContext(ContextStore);
 
   const navigate = useNavigate();
 
-  // State hooks for items and selectionDetails
-  const [items, setItems] = React.useState<IDetailsListBasicExampleItem[]>([]);
-  const [loading, setLoading] = React.useState(true);
+  const {
+    isInitialLoading: loading,
+    data: items,
+    isError,
+    error,
+  } = useQuery(["formLists"], () =>
+    fetchFormList(
+      context.pageContext.user.email || context.pageContext.user.loginName
+    )
+  );
 
-  React.useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true);
-        const itemListPr = sp.web.lists
-          .getById(listId)
-          .items.expand("Author")
-          .select("Author/EMail", "Id", "location", "plantCode", "currStep")
-          .filter("currStep le 3")
-          .get();
-
-        // Get all groups for the current user
-        const userGroupsPr = sp.web.currentUser.groups.select("Title").get(); // : Array<{ Title: string }>
-
-        const [itemList, userGroups] = (await Promise.all([
-          itemListPr,
-          userGroupsPr,
-        ])) as [
-          {
-            Id: number;
-            location: string;
-            plantCode: string;
-            currStep: number;
-          }[],
-          { Title: string }[]
-        ];
-
-        setItems(
-          itemList.map((item) => {
-            const rCurrStep = item.currStep as number;
-            const autherEMail = (item as any).Author.EMail as string;
-
-            let isAuthorized = false;
-            switch (rCurrStep) {
-              case 1:
-                isAuthorized = userGroups.some(
-                  (group) =>
-                    group.Title.toLowerCase() ===
-                    "gm " + item.location.toLowerCase()
-                );
-                break;
-
-              case 2:
-                isAuthorized = userGroups.some(
-                  (group) => group.Title === "PP Department"
-                );
-                break;
-
-              case 3:
-                isAuthorized = userGroups.some(
-                  (group) => group.Title === "GM QC"
-                );
-                break;
-
-              case 0: {
-                // isAuthorized = (context.pageContext.user.email.toLowerCase() || context.pageContext.user.loginName.toLowerCase()).includes(authEMail.toLowerCase());
-                isAuthorized =
-                  autherEMail.toLowerCase() ===
-                  (context.pageContext.user.email.toLowerCase() ||
-                    context.pageContext.user.loginName.toLowerCase());
-                break;
-              }
-
-              default:
-                break;
-            }
-
-            return {
-              ...item,
-              key: item.Id,
-              isAuthorized: Number(isAuthorized),
-            };
-          })
-        );
-      } catch (error) {
-        console.error("Error creating item:", error);
-        navigate("/err/500", { replace: true });
-      } finally {
-        setLoading(false);
-      }
-    })().catch((error) => {
-      console.error("catch external creating item:", error);
-      navigate("/err/500", { replace: true });
-    });
-  }, []);
+  // Error handling
+  if (isError && error instanceof Error) {
+    console.error("Error fetching form list:", error);
+    return (
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h2>Error fetching forms</h2>
+        <p>{(error as Error).message}</p>
+        <PrimaryButton
+          onClick={() => window.location.reload()}
+          iconProps={{ iconName: "Refresh" }}
+        >
+          Refresh
+        </PrimaryButton>
+      </div>
+    );
+  }
 
   // Render output
   return (
@@ -245,8 +261,8 @@ const FormsList: React.FC = () => {
         gap: "20px",
       }}
     >
-      {loading ? (
-        <p>Fetching Details...</p>
+      {loading && !items?.length ? (
+        <Loading label="Fetching..." />
       ) : (
         <>
           <div style={{ marginLeft: "auto" }}>
@@ -259,7 +275,7 @@ const FormsList: React.FC = () => {
           </div>
 
           <DetailsList
-            items={items}
+            items={items!}
             columns={columns}
             setKey="set"
             layoutMode={DetailsListLayoutMode.justified}
